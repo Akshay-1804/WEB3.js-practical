2985eb70b82571e5857a8c43c395f8bbcc8e392c
Akshay Deore <114217670+Akshay-1804@users.noreply.github.com>
2023-06-23 17:23:24 +0530

    Update Simplestorage.js
    As Discussed with Shastri sir

--- Simplestorage.js
+++ Simplestorage.js
@@ -1,33 +1,47 @@
 solc = require("solc");
-fs = require("fs")
-Web3 = require("web3")
-const provider =  new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545")
-web3 = new Web3(provider)
-
-//reading the file 
-file = fs.readFileSync("Simplestorage.sol").toString();
-console.log(file)
-
-
-var input={
+fs=require("fs");
+var { Web3 }=require("web3");
+let web3=new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+let fileContent = fs.readFileSync("Simplestorage.sol").toString();
+console.log(fileContent);
+var input = {
     language: "Solidity",
-    sources:{
-        "Simplestorage.sol":{
-            content:file,
-        },
+    sources: {
+      "Sample.sol": {
+        content: fileContent,
+      },
     },
+  
+    settings: {
+      outputSelection: {
+        "*": {
+          "": [""],
+        },
+      },
 
-    settings:{
-        outputSelection:{
-            "*":["*"], 
-        }
-    }
-}
+    },
+};
+var output=JSON.parse(solc.compile(JSON.stringify(input)));
+console.log(output);
+ABI=output.contracts["Simplestorage.sol"]["Simplestorage"].abi;
+bytecode=output.contracts["Simplestorage.sol"]["Simplestorage"].evm.bytecode.object;
+console.log(ABI);
+console.log(bytecode);
 
-var output = json.parse(solc.compile(JSON.stringify(input)));
-console.log("result:",output);
-
-ABI = output.contracts[Simplestorage.sol]["Simplestorage"].abi;
-bytecode = output.contracts["Simplestorage.sol"]["Simplestorage"].evm.bytecode.object;
-console.log("Bytecode: ",bytecode);
-console.log("ABI : ",ABI);
+contract=new web3.eth.Contract(ABI);
+web3.eth.getAccounts().then((accounts)=>{
+    console.log("Accounts:",accounts);
+    defaultAccount=accounts[0];
+    console.log("Default Account",defaultAccount);
+    contract
+    .deploy({data:bytecode})
+    .send({from:defaultAccount,gas:500000})
+    .on("receipt",(receipt)=>{
+        console.log("Contract Address:",receipt.contractAddress);
+    })
+    .then((SampleContract)=>{
+        SampleContract.methods.x().call((err,data)=>{
+            console.log("Initial value:",data);
+        });
+    });
+});
